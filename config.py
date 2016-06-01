58cc174a546e6f9cbd4f34a32f9a726b51b9ce2e
SommerNicolas <sommer.nicolas92@gmail.com>
2016-06-01 14:08:56 +0200

    Bot capable d'ajouter des fichiers a vérifier et capable de verifier si ces fichiers ont été récemment modifié. Manque: Modification du fichier contenant les dates de dernière modification si celui-ci est contrôlé comme ayant été modifié

---
+++ config.py
@@ -0,0 +1,3 @@
+"""Configuration du bot"""
+DEBUG = True
+TOKEN = "xoxb-38203543493-kEIlWhwpiCS1hRgHazt8gD6O"
---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup d'Alonso"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='BotAlonso',
+    version='1.0',
+    description=__doc__,
+    packages=find_packages()
+)
